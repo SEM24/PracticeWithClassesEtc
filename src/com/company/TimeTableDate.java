0bc8d66a4f7d92f02a04d2704ca5745787a7341b
SEM <samerx777222@gmail.com>
2021-02-14 14:48:04 +0200

    TimeTable class to create time

---
+++ src/com/company/TimeTableDate.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class TimeTableDate {
+
+    private String timeDates;
+
+    public TimeTableDate(String timeDates) {
+        this.timeDates = timeDates;
+    }
+
+    @Override
+    public String toString() {
+        return timeDates;
+    }
+}
